8a905089af91a4f34f14b37bba94883a26323353
AlexHida <96128820+AlexHida@users.noreply.github.com>
2023-09-01 08:28:57 -0500

    registersoluc

--- myapp/forms.py
+++ myapp/forms.py
@@ -13,19 +13,14 @@ class EstudianteForm(forms.ModelForm):
         self.fields['usua'].initial = ''
         self.fields['pass1'].initial = ''
 
-        self.fields['nombre'].label = ''
-        self.fields['apellido'].label = ''
-        self.fields['correo'].label = ''
-        self.fields['usua'].label = ''
-        self.fields['pass1'].label = ''
+        self.fields['nombre'].label = 'Nombre:'
+        self.fields['apellido'].label = 'Apellido:'
+        self.fields['correo'].label = 'Gmail'
+        self.fields['usua'].label = 'Usuario'
+        self.fields['pass1'].label = 'Contraseña'
 
     def clean_usua(self):
         usua = self.cleaned_data.get('usua')
         if Estudiante.objects.filter(usua=usua).exists():
-            raise forms.ValidationError('Este usuario ya está registrado')
-
-    
-    def clean_correo(self):
-        email = self.cleaned_data.get('correo')
-        if Estudiante.objects.filter(correo=email).exists():
-            raise forms.ValidationError('Este correo ya está registrado')
+            raise forms.ValidationError('Este usuario ya está registrado.')
+        return usua
